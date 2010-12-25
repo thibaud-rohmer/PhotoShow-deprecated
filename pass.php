5a4e436f2cbdc90e77ea456df08c5bf6607e2852
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2010-12-25 15:23:21 +0100

    Removed a useless line

--- pass.php
+++ pass.php
@@ -6,7 +6,6 @@ if(!defined("ME_IZ_GOOD")) die("You = bad man.");
 // You may remove this group : it is just an example.
 $groups[]="demo:00000000000000000000000000000000";
 
-$groups[]="plip:cfc73b1859dd9302de92c6e65558156d805336fc";
 
 
 
