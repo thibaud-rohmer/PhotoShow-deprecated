e3b5e55e75162b625abc44086baff46e7ee275e5
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2011-03-06 23:44:51 +0100

    Removed the admin account

--- pass.php
+++ pass.php
@@ -6,13 +6,6 @@ if(!defined("ME_IZ_GOOD")) die("You = bad man.");
 // You may remove this group : it is just an example.
 $groups[]="demo:00000000000000000000000000000000";
 
-$groups[]='admin:d033e22ae348aeb5660fc2140aec35850c4da997';
-$groups[]='plip:cfc73b1859dd9302de92c6e65558156d805336fc';
-$groups[]='plop:64faf5d0b1dc311fd0f94af64f6c296a03045571';
-$groups[]='plup:320c3c32c6fc5700e8510561ff07824e1409ddb5';
-$groups[]='plap:10f72c478fc7e7cd2298e4360e1353cbb80177a7';
-$groups[]='plep:3d89b40265fbb9f4257c1f0e075f1bb3cc17383b';
-$groups[]='ploup:b0ccacdc67a59134ae17172ec79d248eb9e28e1f';
 
 /***** ADD YOUR GROUPS BEFORE THIS LINE ******/
 
