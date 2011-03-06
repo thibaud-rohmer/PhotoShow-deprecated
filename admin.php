68fa64c51bc725ccaf6cb58f95fad0cb4d23fee3
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2011-03-06 23:36:02 +0100

    - Bug fix : Now firefox doesn't have any trouble displaying images in fullscreen mode - Comments : number of comments displayed, and changing the color if there is at least one comment - Multiple levels : It is now possible to have any number of folders, as the displaying of images is done recursively. Folders at a level superior to 2 won't be visible in the interface, but their contents will be displayed - Removing of generated albums : they are now useless (they'll be back when full support of manual creation is working) - Admin interface : it is now possible to dynamically delete images - When logged in, you directly are redirected to the album you wanted to see - Cute URL names o/ - Simplification of some parts of the code

---
+++ admin.php
@@ -0,0 +1,79 @@
+<?php 
+
+if(!isset($_SESSION["logged"])){
+	session_start();
+	$_SESSION["logged"]=true;
+}
+
+require_once "settings.php";
+require_once "functions.php";
+
+define("IS_IN_MY_APP", "TRUE");
+
+$action=$_POST["action"];
+$img=$_POST["img"];
+
+?>
+
+<div class="close">
+	<a href="#">x</a>
+</div>
+
+<?php
+/*
+*  Created by Thibaud Rohmer on 2010-12-23.
+*/
+
+if(isset($_SESSION['groups']))	$groups	= $_SESSION['groups'];
+
+
+if(!isset($groups))  $groups = array();
+
+$authorized[0]="admin";
+if(sizeof(array_intersect($groups,$authorized))==0){
+	echo "Only the admin can view this";
+	return;
+}else{
+	if(file_exists($img) && !is_dir($img))
+	if($action=="delete") {
+		system("rm ".$img);
+		echo("<script>del_select_next();</script>");
+	}
+}
+
+
+echo("
+<script type='text/javascript'>
+
+function doyourstuff(my_action){
+	setup_keyboard();
+	$('#admindiv .content').load('admin.php', {  action: my_action, img: '$img' } );
+}");
+
+?>
+
+$(document).ready(function() {	
+	$('#delete').click(function(){
+		doyourstuff("delete");
+	});
+	
+	
+	$('#admindiv input').focus(function(){
+		remove_keyboard();
+	});
+
+	$(".close").click(function(){
+		$(this).parent().parent().fadeOut("slow");
+	});
+});    
+
+
+</script>
+
+<form accept-charset="utf-8" >	
+	<div id="delete" class="formvalidate">Delete</div>
+</form>
+
+
+
+
