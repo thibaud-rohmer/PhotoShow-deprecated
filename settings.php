a8e54a83f8f6e15ae260d6cf4deaaa352d55db18
Thibaud Rohmer <thibaud.rohmer@gmail.com>
2011-05-12 18:25:15 +0200

    Brought back the settings file to what it should be.

--- settings.php
+++ settings.php
@@ -5,16 +5,4 @@
 
 require "default_settings.php";
 
-// Edit your settings here.
-$theme		=	"snow_white";
-
-$title 		=	"osiPhoto";
-
-
-
-// Right side of menu bar : buttons
-$menubar_right 		=	array("login","help");
-
-
-
 ?>
